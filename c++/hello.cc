6beb61b43263c46e3116576ce600c2cfbfc1ff19
AdityaDolas <dolasadi@gmail.com>
2020-05-07 14:49:15 +0530

    FC

---
+++ c++/hello.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+	cout << "Enter 1st Number: " << endl;
+	cin >> num1;
+	cout << "Enter 2nd Number: " << endl;
+	cin >> num2;
+	cout << num1 + num2 << endl;
+	
+	return 0;
+}
---
+++ node/hello.js
@@ -0,0 +1,5 @@
+console.log("hello world !")
+let campus = [1,2,3,4,5];
+for(let i=0;i<campus.length;i++){
+    console.log(campus[i]);
+}
