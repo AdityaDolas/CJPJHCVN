d8740710a65435b579f1ac36705192f93de5625e
AdityaDolas <dolasadi@gmail.com>
2020-05-14 22:41:36 +0530

    node io

--- node/test.js
+++ node/test.js
@@ -1,9 +1,14 @@
 process.stdin.setEncoding('utf8');
 
+let z = "";
 process.stdin.on('data', function(chunk){
+	z += chunk;
 	process.stdout.write('chunk:' + chunk.toString());
 });
 
 process.stdin.on('end', function(){
+	while (z--){
+		console.log('hi');
+	}
 	process.stdout.write('end');
 });
