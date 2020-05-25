72399177516d2165d20ae887c505080c412b9b7a
AdityaDolas <dolasadi@gmail.com>
2020-05-25 18:00:49 +0530

    S&L3-Method Reference

--- java/StreamLambda.java
+++ java/StreamLambda.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamLambda {
@@ -22,5 +24,26 @@ public class StreamLambda {
 
         list.stream().map(s->s.toUpperCase()).forEach(s-> System.out.println(s));
         list.stream().map(l->l.length()).forEach(l-> System.out.println(l));
+
+        System.out.println("================================================");
+        list.stream().map(String::toUpperCase).forEach(System.out::println);
+
+        int max1 = numbers.stream().max((n1,n2)->Integer.compare(n1,n2)).get();
+        Optional<Integer> max2 = numbers.stream().max(Integer::compareTo);
+        System.out.println(max1);
+        System.out.println(max2.get());
+
+
+        int min1 = numbers.stream().min((n1,n2)->Integer.compare(n1,n2)).get();
+        Optional<Integer> min2 = numbers.stream().min(Integer::compareTo);
+        System.out.println(min1);
+        System.out.println(min2.get());
+
+        List even = numbers.stream().filter(e->e%2==0).collect(Collectors.toList());
+        System.out.println(even);
+
+        List square = numbers.stream().filter(e->e%2==0).map(e->e*e).collect(Collectors.toList());
+        System.out.println(square);
+
     }
 }
